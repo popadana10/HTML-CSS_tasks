b0954294a2da902a7c4ea23b20f9ba2e12f81ae1
Dana Popa <popa.dana100@gmail.com>
2024-02-05 22:34:22 +0200

    some extras

--- portofolio_step2/script.js
+++ portofolio_step2/script.js
@@ -7,3 +7,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+nameInput.addEventListener('input', function() {
+    nameInput.placeholder = '';
+    console.log("Name input field value changed: " + nameInput.value);
+});
+
+emailInput.addEventListener('input', function() {
+    emailInput.placeholder = '';
+    console.log("Email input field value changed: " + emailInput.value);
+});
+
+messageInput.addEventListener('input', function() {
+    messageInput.placeholder = '';
+    console.log("Message input field value changed: " + messageInput.value);
+});
